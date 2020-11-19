34c876e84cb6effbf26935b2d3cdbd59162c3449
Mr'hu <326794739@qq.com>
2020-11-19 16:17:27 +0800

    角色管理完善

--- src/Gardener/Gardener.Client/Pages/UserCenter/Role.razor.cs
+++ src/Gardener/Gardener.Client/Pages/UserCenter/Role.razor.cs
@@ -6,6 +6,7 @@ using AntDesign;
 using AntDesign.TableModels;
 using Gardener.Client.Services;
 using Gardener.Core.Dtos;
+using Mapster;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System;
@@ -17,27 +18,27 @@ namespace Gardener.Client.Pages.UserCenter
 {
     public partial class Role
     {
+        ITable table;
         RoleDto[] roles;
         IEnumerable<RoleDto> selectedRows;
-        ITable table;
+        
         int _pageIndex = 1;
         int _pageSize = 10;
         int _total = 0;
         string _name = string.Empty;
-        bool visible = false;
+        bool drawerVisible = false;
         string drawerTitle = String.Empty;
         bool formIsLoading = false;
         bool tableIsLoading = false;
-        /// <summary>
-        /// form 绑定对象
-        /// </summary>
         RoleDto editModel = new RoleDto();
         [Inject]
         public MessageService MessaheSvr { get; set; }
-        
         [Inject]
         public IRoleService RoleService { get; set; }
-
+        /// <summary>
+        /// 页面初始化完成
+        /// </summary>
+        /// <returns></returns>
         protected override async Task OnInitializedAsync()
         {
 
@@ -55,13 +56,21 @@ namespace Gardener.Client.Pages.UserCenter
             roles = pagedListResult.Items.ToArray();
             tableIsLoading = false;
         }
-
+        /// <summary>
+        /// 查询变化
+        /// </summary>
+        /// <param name="queryModel"></param>
+        /// <returns></returns>
         async Task onChange(QueryModel<RoleDto> queryModel)
         {
             var pagedListResult = await RoleService.Search(_name, _pageIndex, _pageSize);
             roles = pagedListResult.Items.ToArray();
             _total = pagedListResult.TotalCount;
         }
+        /// <summary>
+        /// 点击删除按钮
+        /// </summary>
+        /// <param name="id"></param>
         private async void OnDeleteClick(int id)
         {
             var result = await RoleService.FakeDelete(id);
@@ -73,32 +82,38 @@ namespace Gardener.Client.Pages.UserCenter
             }
         }
         /// <summary>
-        /// 
+        /// 点击编辑按钮
         /// </summary>
         /// <param name="roleDto"></param>
         private async void OnEditClick(RoleDto roleDto)
         {
             drawerTitle = "编辑";
-            editModel.Id = roleDto.Id;
-            editModel.Name = roleDto.Name;
-            editModel.Remark = roleDto.Remark;
+            roleDto.Adapt(editModel);
             await InvokeAsync(StateHasChanged);
-            visible = true;
+            drawerVisible = true;
         }
+        /// <summary>
+        /// 点击添加按钮
+        /// </summary>
         private async void OnAddClick()
         {
-
-            editModel.Id = 0;
-            editModel.Name = string.Empty;
-            editModel.Remark = string.Empty;
+            new RoleDto().Adapt(editModel);
             drawerTitle = "添加";
             await InvokeAsync(StateHasChanged);
-            visible = true;
+            drawerVisible = true;
         }
-        private void Close()
+        /// <summary>
+        /// 抽屉关闭时
+        /// </summary>
+        private void OnDrawerClose()
         {
-            visible = false;
+            drawerVisible = false;
         }
+        /// <summary>
+        /// 表单完成时
+        /// </summary>
+        /// <param name="editContext"></param>
+        /// <returns></returns>
         private async Task OnFormFinish(EditContext editContext)
         {
             formIsLoading = true;
@@ -110,7 +125,7 @@ namespace Gardener.Client.Pages.UserCenter
                 if (role?.Id > 0)
                 {
                     await OnInitializedAsync();
-                    visible = false;
+                    drawerVisible = false;
                     formIsLoading = false;
                     MessaheSvr.Success("添加成功");
                 }
@@ -125,7 +140,7 @@ namespace Gardener.Client.Pages.UserCenter
                 {
                     await OnInitializedAsync();
                     MessaheSvr.Success("修改成功", 1);
-                    visible = false;
+                    drawerVisible = false;
                 }
                 formIsLoading = false;
 
@@ -133,23 +148,33 @@ namespace Gardener.Client.Pages.UserCenter
             }
 
         }
+        /// <summary>
+        /// 表单失败时
+        /// </summary>
+        /// <param name="editContext"></param>
         private void OnFormFinishFailed(EditContext editContext)
         {
-            //visible = false;
+            //drawerVisible = false;
         }
+        /// <summary>
+        /// 表单取消
+        /// </summary>
         private void OnFormCancel()
         {
-            editModel.Id = 0;
-            editModel.Name = string.Empty;
-            editModel.Remark = string.Empty;
-            visible = false;
+            new RoleDto().Adapt(editModel);
+            drawerVisible = false;
         }
-
+        /// <summary>
+        /// 点击删除选中按钮
+        /// </summary>
         private async void OnDeletesClick()
         {
             DeleteSelected();
         }
-        
+        /// <summary>
+        /// 删除选中
+        /// </summary>
+        /// <returns></returns>
         private async Task DeleteSelected()
         {
             if (selectedRows == null || selectedRows.Count() == 0)
