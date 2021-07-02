b92a457774b5e5ebd0ce994ec14a5111cc97e65a
Mr'hu <326794739@qq.com>
2021-07-02 13:23:27 +0800

    增加了可以根据内容变化颜色的 tag标签

---
+++ src/Gardener/Gardener.Client/Components/TagRandomColor.razor.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------------
+// 园丁,是个很简单的管理系统
+//  gitee:https://gitee.com/hgflydream/Gardener 
+//  issues:https://gitee.com/hgflydream/Gardener/issues 
+// -----------------------------------------------------------------------------
+
+using AntDesign;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Gardener.Client.Components
+{
+    public partial class TagRandomColor : AntDomComponentBase
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public object Text { get; set; }
+
+        private string color = "";
+
+        private string[] colors = { "magenta", "pink", "red", "volcano", "orange", "green", "cyan", "blue", "lime", "geekblue", "purple" };
+
+        protected override void OnInitialized()
+        {
+            int code = Math.Abs(Text.ToString().GetHashCode());
+            int colorIndex = (code % 1000) % colors.Length;
+
+            color = colors[colorIndex];
+            Console.WriteLine( code + "_" + colors.Length + "_" + colorIndex + "_" + color);
+
+        }
+
+    }
+}
