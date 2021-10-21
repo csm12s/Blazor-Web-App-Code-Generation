1f4eef31961b61c0d44a22b33ce490b42a3e6971
hhf <huhangfeivip@163.com>
2021-10-21 19:38:34 +0800

    添加多标签

---
+++ src/Client/Gardener.Client.Entry/Pages/Home.razor.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------------------------------------------
+// 园丁,是个很简单的管理系统
+//  gitee:https://gitee.com/hgflydream/Gardener 
+//  issues:https://gitee.com/hgflydream/Gardener/issues 
+// -----------------------------------------------------------------------------
+
+using Gardener.Client.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gardener.Client.Entry.Pages
+{
+    public partial class Home : ReuseTabsPageBase
+    {
+    }
+}
