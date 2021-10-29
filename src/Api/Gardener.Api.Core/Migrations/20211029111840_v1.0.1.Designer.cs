250abf712ffcdd8639445632149cbf91fd52d33b
hhf <huhangfeivip@163.com>
2021-10-29 19:23:22 +0800

    update

--- src/Api/Gardener.Api.Core/Migrations/20211029111840_v1.0.1.Designer.cs
+++ src/Api/Gardener.Api.Core/Migrations/20211029111840_v1.0.1.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Gardener.EntityFramwork.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Gardener.Api.Core.Migrations
 {
     [DbContext(typeof(GardenerDbContext))]
-    partial class GardenerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211029111840_v1.0.1")]
+    partial class v101
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
