c41498ebaa7a9ee47744bbfd0729fdcfa4e45206
hhf <huhangfeivip@163.com>
2023-01-09 14:38:09 +0800

    feat: 引入BlazorMonaco，优化种子数据显示效果

--- src/Modules/UserCenter/Gardener.UserCenter.Client/Pages/RoleView/Role.razor.cs
+++ src/Modules/UserCenter/Gardener.UserCenter.Client/Pages/RoleView/Role.razor.cs
@@ -9,13 +9,16 @@ using Gardener.Base.Resources;
 using Gardener.Client.Base;
 using Gardener.Client.Base.Components;
 using Gardener.UserCenter.Dtos;
+using Gardener.UserCenter.Services;
+using Microsoft.AspNetCore.Components;
 using System.Threading.Tasks;
 
 namespace Gardener.UserCenter.Client.Pages.RoleView
 {
     public partial class Role : ListOperateTableBase<RoleDto, int, RoleEdit>
     {
-
+        [Inject]
+        public IRoleService roleService { get; set; }
         /// <summary>
         /// 点击分配资源
         /// </summary>
@@ -32,10 +35,8 @@ namespace Gardener.UserCenter.Client.Pages.RoleView
         /// <returns></returns>
         private async Task OnDownloadClick()
         {
-            await OpenOperationDialogAsync<RoleResourceDownload, string, bool>(
-                localizer[SharedLocalResource.SeedData],
-                      string.Empty,
-                       width: 1300);
+            string seedData = await roleService.GetRoleResourceSeedData();
+            await OpenOperationDialogAsync<ShowSeedDataCode, string, bool>(localizer[SharedLocalResource.SeedData], seedData, width: 1300);
         }
 
     }
