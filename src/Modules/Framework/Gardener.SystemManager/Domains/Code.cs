29b5486600f4344c93774233e042f0dbb6763781
Mr'hu <326794739@qq.com>
2023-04-05 22:07:47 +0800

    feat:add code、codeType entity

---
+++ src/Modules/Framework/Gardener.SystemManager/Domains/Code.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------------
+// 园丁,是个很简单的管理系统
+//  gitee:https://gitee.com/hgflydream/Gardener 
+//  issues:https://gitee.com/hgflydream/Gardener/issues 
+// -----------------------------------------------------------------------------
+
+using Gardener.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gardener.SystemManager.Domains
+{
+    /// <summary>
+    /// 字典信息
+    /// </summary>
+    public class Code : GardenerEntityBase
+    {
+
+    }
+}
---
+++ src/Modules/Framework/Gardener.SystemManager/Domains/CodeType.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------------
+// 园丁,是个很简单的管理系统
+//  gitee:https://gitee.com/hgflydream/Gardener 
+//  issues:https://gitee.com/hgflydream/Gardener/issues 
+// -----------------------------------------------------------------------------
+
+using Gardener.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gardener.SystemManager.Domains
+{
+    /// <summary>
+    /// 字典类型
+    /// </summary>
+    public class CodeType : GardenerEntityBase
+    {
+        /// <summary>
+        /// 字典类型名称
+        /// </summary>
+        [DisplayName("字典类型名称")]
+        [Required, MaxLength(50)]
+        public string CodeTypeName { get; set; } = null!;
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [DisplayName("字典类型名称")]
+        public string? Remark { get; set; }
+    }
+}
