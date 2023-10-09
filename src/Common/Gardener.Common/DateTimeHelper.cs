c2852dc3ddd0a1985e2d745800ea6c74f28dafb7
hhf <huhangfeivip@163.com>
2023-10-09 14:51:55 +0800

    feat:增加开发工具-时间戳转换

--- src/Common/Gardener.Common/DateTimeHelper.cs
+++ src/Common/Gardener.Common/DateTimeHelper.cs
@@ -26,5 +26,24 @@ namespace Gardener.Common
             string sdt2 = string.Format("{0}:{1}:{2}:{3}", ts.Days.ToString().PadLeft(2, '0'), ts.Hours.ToString().PadLeft(2, '0'), ts.Minutes.ToString().PadLeft(2, '0'), ts.Seconds.ToString().PadLeft(2, '0'));
             return sdt2;
         }
+        /// <summary>
+        /// 时间转时间戳(毫秒级)
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static long DateTimeToTimestamp(DateTime time)
+        {
+            TimeSpan ts = time - new DateTime(1970, 1, 1).ToLocalTime();
+            return Convert.ToInt64(ts.TotalMilliseconds);
+        }
+        /// <summary>
+        /// 时间戳(毫秒级)转时间
+        /// </summary>
+        /// <param name="timestamp"></param>
+        /// <returns></returns>
+        public static DateTime TimestampToDateTime(long timestamp)
+        {
+            return new DateTime(1970, 1, 1).ToLocalTime().AddMilliseconds(timestamp);
+        }
     }
 }
